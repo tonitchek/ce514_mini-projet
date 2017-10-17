dcc3453fbbfd3b06d468d39aef2393e22b71d8bc
broquet <broquet@esrf.fr>
2017-10-17 17:09:32 +0200

    hdl: add generic counter in tb_m4x4_mult testbench

--- hdl/tb/tb_m4x4_mult.vhd
+++ hdl/tb/tb_m4x4_mult.vhd
@@ -34,12 +34,30 @@ architecture Behavorial of tb_m4x4_mult is
   signal matc_rows2_o : mat_1x4_18bits;
   signal matc_rows3_o : mat_1x4_18bits;
 
+  signal ena_timer_i : std_logic;
+  signal clr_timer_i : std_logic;
+  signal aut_timer_i : std_logic;
+  signal ena_timer   : std_logic;
+  signal clr_timer   : std_logic;
+  signal timer_cnt   : std_logic_vector(31 downto 0);
+
+  component generic_counter is
+    generic (
+      g_width : integer := 32
+      );
+    port (
+      clk_i : in  std_logic;
+      rst_i : in  std_logic;
+      ena_i : in  std_logic;
+      clr_i : in  std_logic;
+      ofw_o : out std_logic;
+      cnt_o : out std_logic_vector(g_width - 1 downto 0)
+      );
+  end component;
+
 begin
 
-  uut:m4x4_mult
-    generic map (
-      g_simulation => true
-      )
+  uut1:m4x4_mult
     port map (
       clk_i => clk_i,
       rst_i => rst_i,
@@ -54,11 +72,28 @@ begin
       done_o => done_o
       );
 
+  clr_timer <= start_i when aut_timer_i = '1' else clr_timer_i;
+  ena_timer <= not done_o when aut_timer_i = '1' else ena_timer_i;
+  
+  uut2:generic_counter
+    generic map (
+      g_width => 32
+      )
+    port map (
+      clk_i => clk_i,
+      rst_i => rst_i,
+      ena_i => ena_timer,
+      clr_i => clr_timer,
+      ofw_o => open,
+      cnt_o => timer_cnt
+      );
+
+  
   clock:process
   begin
-    clk_i <= '0';
-    wait for clk_period/2;
     clk_i <= '1';
+    wait for clk_period/2;
+    clk_i <= '0';
     wait for clk_period/2;
   end process;
 
@@ -67,19 +102,41 @@ begin
     rst_i <= '0';
     inhibit_i <= '1';
     start_i <= '0';
-    wait for 13 ns;
+    ena_timer_i <= '0';
+    clr_timer_i <= '0';
+    aut_timer_i <= '0';
+    wait for 11 ns;
     rst_i <= '1';
     wait for 52 ns;
     rst_i <= '0';
-    wait for 72 ns;
+    wait for 73 ns;
     start_i <= '1';
     wait for clk_period;
     start_i <= '0';
     inhibit_i <= '0';
     wait for 123 ns;
+    ena_timer_i <= '1';
+    wait for 133 ns;
     start_i <= '1';
     wait for clk_period;
     start_i <= '0';
+    wait for 23 ns;
+    ena_timer_i <= '0';
+    wait for 135 ns;
+    clr_timer_i <= '1';
+    wait for clk_period;
+    clr_timer_i <= '0';
+    wait for 48 ns;
+    aut_timer_i <= '1';
+    wait for 78 ns;
+    start_i <= '1';
+    wait for clk_period;
+    start_i <= '0';
+    wait for 172 ns;
+    start_i <= '1';
+    wait for clk_period;
+    start_i <= '0';
+    
     wait for 53 ns;
     rst_i <= '1';
     wait for 23 ns;
